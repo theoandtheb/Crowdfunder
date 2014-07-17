b539d9729a52c5a760962402ccd01de17cd1a278
theoandtheb <blue.apple@sympatico.ca>
2014-07-17 14:39:28 -0400

    Interm merge; db conflict resolved while working on user_project association.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140716195905) do
+ActiveRecord::Schema.define(version: 20140717181457) do
 
   create_table "pledges", force: true do |t|
     t.integer  "amount"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20140716195905) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "category"
+    t.integer  "user_id"
   end
 
   create_table "rewards", force: true do |t|
